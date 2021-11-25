599fbf13f8d12bff479a3f328ac079749ee760cb
psilore <psiloc@gmail.com>
2021-11-25 22:21:56 +0100

    update

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./main.js",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./index.html",
+          to: `${__dirname}/dist/index.html`,
+        }
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new HtmlMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "bundle.js",
+  },
+  mode: "production",
+  module: {},
+};
